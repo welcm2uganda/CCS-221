2e3d046521a4f8a79a51c562ca2a5de900f1a605
welcm2uganda <126628496+welcm2uganda@users.noreply.github.com>
2023-03-10 15:01:16 +0800

    Update Activity_1_DDA_Line_Midpoint.py

--- pages/Activity_1/Activity_1_DDA_Line_Midpoint.py
+++ pages/Activity_1/Activity_1_DDA_Line_Midpoint.py
@@ -2,33 +2,53 @@ import streamlit as st
 import numpy as np
 import matplotlib.pyplot as plt
 
-st.title("Activity\t1\nGroup\t8\tBYTE\nDDA\tMidpoint")
+st.title("Activity\t1\nGroup\t8\tBYTE\nBresenham\tMidpoint")
 
-def DDALine(x1, y1, x2, y2, color):
-    dx = x2 - x1
-    dy = y2 - y1
-    x3 = (x1 + x2) / 2
-    y3 = (y1 + y2) / 2
+def BresenhamLine(x1, y1, x2, y2, color):
+    
+    x, y = x1, y1
+    dx = abs(x2 - x1)
+    dy = abs(y2 - y1)
+    gradient = dy/float(dx)
+    x3 = (x2 + x1) / 2
+    y3 = (y2 + y1) / 2
+   
 
+    if gradient > 1:
+        dx, dy = dy, dx
+        x, y = y, x
+        x1, y1 = y1, x1
+        x2, y2 = y2, x2
+
+    p = 2 * dy - dx
+    xcoordinates = [x]
+    ycoordinates = [y]
+
+    
     steps = abs(dx) if abs(dx) > abs(dy) else abs(dy)
-
-    Xcoordinate = float(dx / steps)
-    Ycoordinate = float(dy / steps)
 
     fig = plt.figure()
     for i in range(0, int(steps + 1)):
+        
+        if p > 0:
+            y = y + 1 if y < y2 else y - 1
+            p = p + 2 * (dy - dx)
+        else:
+            p = p + 2  * dy
+
+        x = x + 1 if x < x2 else x - 1
 
         print('x = %s, y = %s' % (x, y))
-        plt.plot(int(x1), int (y1), color)
-        x1 += Xcoordinate
-        y1 += Ycoordinate
+        xcoordinates.append(x)
+        ycoordinates.append(y)            
+    plt.plot(xcoordinates, ycoordinates)
     plt.plot(x3, y3, marker = "o", markersize = 5, markerfacecolor = "red")
     plt.show()
     st.pyplot(fig)
     st.write("Midpoint: ", int(x3), ", ", int(y3), ".")
 
 def main():
-    st.title("DDA Line")
+    st.title("Bresenham Line")
     x1 = 10
     x2 = 10
 
@@ -52,7 +72,8 @@ def main():
         0, 100)
     st.write('y2: ', y2)
     color = "g." 
-    DDALine(x, y, x2, y2, color)
+    BresenhamLine(x, y, x2, y2, color)
+
 
 if __name__ == '__main__':
     main()
