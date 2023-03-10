c4a34d6cdfe57bcffa5f027e4531abbc275fe25e
welcm2uganda <126628496+welcm2uganda@users.noreply.github.com>
2023-03-10 14:13:59 +0800

    Update Activity_1_DDA_Line_Midpoint.py

--- pages/Activity_1/Activity_1_DDA_Line_Midpoint.py
+++ pages/Activity_1/Activity_1_DDA_Line_Midpoint.py
@@ -1,4 +1,8 @@
+import streamlit as st
+import numpy as np
 import matplotlib.pyplot as plt
+
+st.title("Activity\t1\nGroup\t8\tBYTE\nDDA\tMidpoint")
 
 def DDALine(x1, y1, x2, y2, color):
     dx = x2 - x1
@@ -11,20 +15,43 @@ def DDALine(x1, y1, x2, y2, color):
     Xcoordinate = float(dx / steps)
     Ycoordinate = float(dy / steps)
 
+    fig = plt.figure()
     for i in range(0, int(steps + 1)):
 
+        print('x = %s, y = %s' % (x, y))
         plt.plot(int(x1), int (y1), color)
         x1 += Xcoordinate
         y1 += Ycoordinate
     plt.plot(x3, y3, marker = "o", markersize = 5, markerfacecolor = "red")
     plt.show()
+    st.pyplot(fig)
+    st.write("Midpoint: ", int(x3), ", ", int(y3), ".")
 
 def main():
-    x = int(input("Enter X1: "))
-    y = int(input("Enter Y1: "))
-    xEnd = int(input("Enter X2: "))
-    yEnd = int(input("Enter Y2: "))
-    color = "g."
+    st.title("DDA Line")
+    x1 = 10
+    x2 = 10
+
+    x = st.slider(
+        'X1',
+        0, 100)
+    st.write('x1: ', x)
+
+    y = st.slider(
+        'Y1',
+        0, 100)
+    st.write('y1: ', y)
+
+    x2 = st.slider(
+        'X2',
+        0, 100)
+    st.write('x2: ', x2)
+
+    y2 = st.slider(
+        'Y2',
+        0, 100)
+    st.write('y2: ', y2)
+    color = "g." 
     DDALine(x, y, xEnd, yEnd, color)
 
 if __name__ == '__main__':
